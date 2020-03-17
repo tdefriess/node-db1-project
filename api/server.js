a4f7eeb88e37e02377a0e7b48c1cd7331fbc178b
Tyler de Friess <tyler.defriess@gmail.com>
2020-03-17 11:39:45 -0400

    put added

--- api/server.js
+++ api/server.js
@@ -44,4 +44,22 @@ server.post('/', (req, res) => {
         })
 })
 
+server.put('/:id', (req, res) => {
+    const changes = req.body;
+
+    db("accounts")
+        .where({ id: req.params.id })
+        .update(changes)
+        .then(count => {
+            if (count > 0) {
+                res.status(200).json({message: 'record updated successfully'});
+            } else {
+                res.status(404).json({message: 'That record does not exist'});
+            }
+        })
+        .catch(err => {
+            res.status(500).json({message: "database error"})
+        })
+})
+
 module.exports = server;
