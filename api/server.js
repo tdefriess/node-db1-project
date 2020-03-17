49040360a00b147bd2d34c364717e4474169e8af
Tyler de Friess <tyler.defriess@gmail.com>
2020-03-17 11:28:28 -0400

    first endpoint working

--- api/server.js
+++ api/server.js
@@ -6,4 +6,15 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/', (req, res) => {    
+    db.select("*")
+        .from("accounts")
+        .then(rows => {
+            res.status(200).json({ data: rows });
+        })
+        .catch(err => {
+            res.status(500).json({ message: "sorry, ran into an error"});
+        })
+})
+
 module.exports = server;
